92454e197efe082c4ab377bdcef668e771af79cb
kkkbcsooorichhh <kingblechen8888@Gmail.com>
2023-04-28 01:37:09 +0800

    vvv

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './electrolyteproject/',
+  base: 'https://kkkbcsooorichhh.github.io/electrolyteproject/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
